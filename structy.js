f84b57fd160c8f7fc570439764bfbc606b4f273c
Tom Li <tomleslieli@yahoo.com>
2023-04-19 17:43:16 -0400

    More problems

--- structy.js
+++ structy.js
@@ -182,3 +182,25 @@
 //   };
 
 ////////// FIVE SORT  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// const fiveSort = (nums) => {
+//     let i = 0;
+//     let j = nums.length - 1;
+    
+//     while (i <= j){
+//       if (nums[j] === 5){
+//         j -= 1;
+//       } else if (nums[i] === 5){
+//         [nums[i], nums[j]] = [nums[j], nums[i]]
+//         i += 1;
+//       } else {
+//         i += 1;
+//       }
+//     }
+//     return nums;
+//   };
+  
+//   module.exports = {
+//     fiveSort,
+//   };
+  
