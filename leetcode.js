aa01e23c9a509ae7c6f1d12c0baea88cde81c838
Tom Li <tomleslieli@yahoo.com>
2023-05-26 18:06:17 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -131,3 +131,94 @@ const gridIllumination = function (N, lamps, queries) {
       map.set(value, map.get(value) - 1)
     }
   }
+
+  ////////// FIND COMMON CHARACTERS   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const commonChars = function(A) {
+    const minArr = minEl(A)
+    const res = []
+    for(let i = 0; i < minArr[1]; i++) {
+      let target = A[minArr[0]][i]
+      let all = true
+      for(let j = 0; j < A.length; j++) {
+        if(j === minArr[0]) continue
+        if(all === false) continue
+        let idx
+        if( (idx = A[j].indexOf(target)) === -1) {
+           all = false
+        } else {
+          A[j] = A[j].slice(0, idx) + A[j].slice(idx + 1)
+        }
+      }
+      if(all) res.push(target)
+    }
+        
+    return res
+};
+
+function minEl(arr) {
+  const res = [0, Number.MAX_SAFE_INTEGER] // [idx, len]
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].length < res[1]) {
+       res[0] = i
+       res[1] = arr[i].length
+    }
+  }
+  return res
+}
+
+  ////////// COMPLEMENT OF BASE 10 INTEGER   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const bitwiseComplement = function (N) {
+    if (N === 0) return 1
+    let bitmask = N
+    bitmask |= bitmask >> 1
+    bitmask |= bitmask >> 2
+    bitmask |= bitmask >> 4
+    bitmask |= bitmask >> 8
+    bitmask |= bitmask >> 16
+    return bitmask ^ N
+  }
+
+  ////////// MAX CONSECUTIVE ONES   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  const longestOnes = function (A, K) {
+    let i = 0
+    let j = 0
+    const len = A.length
+    while (j < len) {
+      if (A[j] === 0) K--
+      if (K < 0) {
+        if (A[i] === 0) K++
+        i++
+      }
+      j++
+    }
+    return j - i
+  }
+
+    ////////// MAXIMIZE SUM OF ARRAY AFTER NEGATIONS   ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    const largestSumAfterKNegations = function(A, K) {
+      if (A.length === 0) return 0;
+      A.sort((a, b) => a - b);
+      let res = 0;
+      let posIdx;
+      for (let i = 0, num = 0; i < A.length; i++) {
+        if (num < K) {
+          if (A[i] < 0) {
+            A[i] = -A[i];
+          } else {
+            if (posIdx == null) {
+              posIdx = Math.abs(A[i]) - Math.abs(A[i - 1]) > 0 ? i - 1 : i;
+            }
+            A[posIdx] = -A[posIdx];
+          }
+          num++;
+        }
+      }
+      res = A.reduce((ac, el) => ac + el, 0);
+      return res;
+    };
+
+
