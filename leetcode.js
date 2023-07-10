1c0f64781faf73228c18b2ada1012910d20c1128
Tom Li <tomleslieli@yahoo.com>
2023-07-10 12:14:06 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -1671,3 +1671,57 @@ const maximumScore = function(nums, multipliers) {
   }
 
 };
+
+
+////////// MAXIMIZE PALINDROME LENGTH FROM SUBSEQUENCES ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const longestPalindrome = function(word1, word2) {
+  const str = word1 + word2
+  const len = str.length, m = word1.length, n = word2.length
+  const dp = LPS(str)
+  let res = 0
+  for(let i = 0; i < m; i++) {
+    for(let j = 0; j < n; j++) {
+      if(word1[i] !== word2[j]) continue
+      res = Math.max(res, 2 + dp[i + 1][j + m - 1])
+    }
+  }
+  return res
+}
+
+function LPS(str) {
+  const n = str.length
+  const dp = Array.from({ length: n }, () => Array(n).fill(0))
+  for(let i = n - 1; i >= 0; i--) {
+    dp[i][i] = 1
+    for(let j = i + 1; j < n; j++) {
+      if(str[i] === str[j]) dp[i][j] = 2 + dp[i + 1][j - 1]
+      else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1])
+    }
+  }
+  return dp
+}
+
+////////// COUNT ITEMS MATCHING A RULE ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const countMatches = function(items, ruleKey, ruleValue) {
+  let res = 0
+  for(let e of items) {
+    if(helper(e, ruleKey, ruleValue)) res++
+  }
+  return res
+};
+
+function helper(e, k, v) {
+  const [t, c, n] = e
+  if(k === 'type' && v === t) {
+    return true
+  } else if(k === 'color' && v === c) {
+    return true
+  } else if(k === 'name' && v === n) {
+    return true
+  }
+  
+  return false
+  
+}
