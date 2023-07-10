5193955d654374fe1d7bb51e6b83096673defc3f
Tom Li <tomleslieli@yahoo.com>
2023-07-10 12:13:03 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -1645,3 +1645,29 @@ function helper(str, idx) {
   return res
 }
 
+////////// NTH HIGHEST SALARY IN SQL ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      select distinct e1.salary from Employee e1 where N-1 = (select count(distinct e2.Salary) from Employee e2 where e1.Salary < e2.Salary)
+  );
+END
+
+////////// MAX SCORE FROM PERFORMING MULTIPLICATION OPERATIONS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const maximumScore = function(nums, multipliers) {
+  const n = nums.length, m = multipliers.length
+  const { max } = Math
+  const dp = Array.from({ length: m + 1 }, () => Array(m + 1).fill(-Infinity))
+  return helper(0, 0)
+  function helper(l, i) {
+    if(i === m) return 0
+    if(dp[l][i] !== -Infinity) return dp[l][i]
+    const pickLeft = helper(l + 1, i + 1) + nums[l] * multipliers[i]
+    const pickRight = helper(l, i + 1) + nums[n - (i - l) - 1] * multipliers[i]
+    return dp[l][i] = max(pickLeft, pickRight)
+  }
+
+};
