2b14cc33960bbbb052a5e49703da8b5fff9bc226
Tom Li <tomleslieli@yahoo.com>
2023-07-12 11:28:42 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -1825,3 +1825,40 @@ const nearestValidPoint = function(x, y, points) {
   
   return idx === -1 ? -1 : idx
 };
+
+////////// LONGEST HAPPY PREFIX ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+const longestPrefix = function(s) {
+  return s.slice(0, dfa().pop())
+  function dfa() {
+    let i = 1
+    let j = 0
+    const len = s.length
+    const prefix = Array(len + 1).fill(0)
+    prefix[0] = -1
+    prefix[1] = 0
+    while(i < len) {
+      if(s[j] === s[i]) {
+        j++
+        i++
+        prefix[i] = j
+      } else {
+        if(j > 0) j = prefix[j]
+        else i++
+      }
+    }
+    return prefix
+  }  
+};
+
+////////// CAPITAL GAIN LOSS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+SELECT stock_name, SUM(
+  CASE
+      WHEN operation = 'Buy' THEN -price
+      ELSE price
+  END
+) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name
