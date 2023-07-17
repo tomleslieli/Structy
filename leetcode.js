3c1e59556a27648639fa0d8821b3ddfa74a99cd5
Tom Li <tomleslieli@yahoo.com>
2023-07-16 23:50:08 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -2008,3 +2008,67 @@ const canBeEqual = function(target, arr) {
   
   return true
 };
+
+
+////////// CHECK IF A STRING CONTAINS ALL BINARY CODES OF SIZE K ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const hasAllCodes = function (s, k) {
+  if (s.length < k) return false
+  const set = new Set()
+  for (let i = 0; i <= s.length - k; i++) {
+    set.add(s.slice(i, i + k))
+  }
+
+  return set.size == Math.pow(2, k)
+}
+
+////////// COURSE SCHEDULE ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const checkIfPrerequisite = function (numCourses, prerequisites, queries) {
+  const n = numCourses, m = prerequisites.length
+  const graph = {}, inDegree = Array(n).fill(0)
+  
+  for(const [s, e] of prerequisites) {
+    if(graph[s] == null) graph[s] = []
+    inDegree[e]++
+    graph[s].push(e)
+  }
+  
+  let q = []
+  
+  for(let i = 0; i < n; i++) {
+    if(inDegree[i] === 0) q.push(i)
+  }
+  
+  const hash = {}
+
+  while(q.length) {
+    const size = q.length
+    const nxt = []
+    for(let i = 0; i < size; i++) {
+      const cur = q[i]
+      for(const e of (graph[cur] || [])) {
+        inDegree[e]--
+        if(hash[e] == null) hash[e] = new Set()
+        hash[e].add(cur)
+        for(const dep of (hash[cur] || [])) {
+          hash[e].add(dep)
+        }
+      
+        if(inDegree[e] === 0) {
+          nxt.push(e)
+        }
+      }
+    }
+    
+    q = nxt
+  }
+  
+  const res = []
+  for(const [p, e] of queries) {
+    if(hash[e] && hash[e].has(p)) res.push(true)
+    else res.push(false)
+  }
+  
+  return res
+}
