253455c3aae695f7d78218f6df3fa42c81685d2b
Tom Li <tomleslieli@yahoo.com>
2023-07-21 23:22:50 -0400

    Some Problems

--- leetcode.js
+++ leetcode.js
@@ -2072,3 +2072,40 @@ const checkIfPrerequisite = function (numCourses, prerequisites, queries) {
   
   return res
 }
+
+//////////  FINAL PRICES WITH A SPECIAL DISCOUNT ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const finalPrices = function(prices) {
+  const res = [], n = prices.length
+  for(let i = 0; i < n; i++) {
+    const cur = prices[i]
+    let dis = null
+    for(let j = i + 1; j < n; j++) {
+      if(prices[j] <= cur) {
+        dis = prices[j]
+        break
+      }
+    }
+    res.push(dis == null ? cur : cur - dis)
+  }
+  return res
+};
+
+//////////  SUBRECTANGLE QUERIES ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const SubrectangleQueries = function(rectangle) {
+  this.rect = rectangle
+  this.ops = []
+};
+
+SubrectangleQueries.prototype.updateSubrectangle = function(row1, col1, row2, col2, newValue) {
+  this.ops.push([row1, col1, row2, col2, newValue])
+};
+
+SubrectangleQueries.prototype.getValue = function(row, col) {
+  for(let i = this.ops.length - 1; i >= 0; i--) {
+    const op = this.ops[i]
+    if(op[0] <= row && op[1] <= col && row <= op[2] && col <= op[3]) return op[4]
+  }
+  return this.rect[row][col]
+};
