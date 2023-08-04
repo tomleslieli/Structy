84a7e6faf1eadb11973641feb3cc86c64979c39a
Tom Li <tomleslieli@yahoo.com>
2023-08-03 21:27:07 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -2584,3 +2584,25 @@ const findingUsersActiveMinutes = function(logs, k) {
   return res
   
 };
+
+////////// NUMBER OF DIFFERENT SUBSEQUENCES GCDS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const countDifferentSubsequenceGCDs = function(nums) {
+  const MAX = 2e5 + 1;
+   const cnt = Array(MAX).fill(0)
+  for (let x of nums) cnt[x] = true;
+  let ret = 0;
+  for (let x=1; x<MAX; x++) {
+      let g = 0;
+      for (let y=x; y<MAX; y+=x) {
+          if (cnt[y]) g = gcd(g, y);
+      }
+      if (g == x) ret++;
+  }
+  return ret;
+};
+
+function gcd(x,y){
+  if(y === 0) return x
+  return gcd(y, x % y)
+}
