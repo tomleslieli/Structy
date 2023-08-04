65900ff0209a91c6f7ec8020b4c66512fe386835
Tom Li <tomleslieli@yahoo.com>
2023-08-03 21:24:56 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -2521,3 +2521,36 @@ const evaluate = function(s, knowledge) {
   return arr.join('')
   
 };
+
+////////// MAXIMIZE NUMBER OF NICE DIVISORS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const MOD = BigInt(1e9 + 7)
+const maxNiceDivisors = (pf) => {
+  if (pf == 1) return 1
+  let bpf = BigInt(pf)
+  let res
+  if (pf % 3 == 0) {
+    res = powmod(3n, bpf / 3n, MOD)
+  } else if (pf % 3 == 1) {
+    res = (powmod(3n, bpf / 3n - 1n, MOD) * 4n) % MOD
+  } else {
+    res = (powmod(3n, bpf / 3n, MOD) * 2n) % MOD
+  }
+  return Number(res)
+}
+
+const powmod = (a, b, mod) => {
+  let r = 1n
+  while (b > 0n) {
+    if (b % 2n == 1) r = (r * a) % mod
+    b >>= 1n
+    a = (a * a) % mod
+  }
+  return r
+}
+
+////////// EMPLOYEES EARNING MORE THAN THEIR MANAGERS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+SELECT Name AS Employee FROM Employee AS E,
+(SELECT DISTINCT Id, Salary FROM Employee) AS M
+WHERE E.ManagerId = M.Id AND E.Salary > M.Salary
