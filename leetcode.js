704fddc264784fa618b99c6dfd9a68de597e9765
Tom Li <tomleslieli@yahoo.com>
2023-09-16 03:45:53 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -3583,3 +3583,25 @@ const truncateSentence = function(s, k) {
   const sli = arr.slice(0, k)
   return sli.join(' ')
 };
+
+////////// FINDING THE USERS ACTIVE MINUTES ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const findingUsersActiveMinutes = function(logs, k) {
+  const hash = {}, map = {}
+  logs.forEach(l => {
+    const [id, mi] = l
+    if(hash[mi] == null) hash[mi] = new Set()
+    if(map[id] == null) map[id] = new Set()
+    hash[mi].add(id)
+    map[id].add(mi)
+  })
+
+  const res = Array(k).fill(0)
+  Object.keys(map).forEach(k => {
+     const num = map[k].size
+     res[num - 1]++
+  })
+  
+  return res
+  
+};
