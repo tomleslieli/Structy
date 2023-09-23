56e74f932c63d9b172a023e3d08f3069c7776ca9
Tom Li <tomleslieli@yahoo.com>
2023-09-23 16:47:29 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -3748,3 +3748,33 @@ const getBiggestThree = function (grid) {
   res.sort((a, b) => b - a)
   return res
 }
+
+////////// MINIMUM XOR SUM OF TWO ARRAYS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const minimumXORSum = function (nums1, nums2) {
+  const n = nums1.length
+  const limit = 1 << n
+  const dp = Array(limit).fill(Infinity)
+  dp[0] = 0
+  for (let mask = 1; mask < limit; ++mask) {
+    for (let i = 0; i < n; ++i) {
+      if ((mask >> i) & 1) {
+        dp[mask] = Math.min(
+          dp[mask],
+          dp[mask ^ (1 << i)] + (nums1[bitCnt(mask) - 1] ^ nums2[i])
+        )
+      }
+    }
+  }
+  return dp[limit - 1]
+}
+
+function bitCnt(num) {
+  let res = 0
+  while (num) {
+    res++
+    num = num & (num - 1)
+  }
+
+  return res
+}
