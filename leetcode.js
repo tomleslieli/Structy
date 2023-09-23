fa632cee2a0993e0207022ef7f453aee39ce3447
Tom Li <tomleslieli@yahoo.com>
2023-09-23 16:46:09 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -3692,3 +3692,19 @@ const minProductSum = function(nums1, nums2) {
   return res
 };
 
+////////// SUBSTRINGS OF SIZE THREE WITH DISTINCT CHARACTERS ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const countGoodSubstrings = function(s) {
+  let res = 0
+  for(let i = 2; i < s.length; i++) {
+    if(chk(s, i)) res++
+  }
+  
+  return res
+};
+
+function chk(s, i) {
+  return s[i - 2] !== s[i - 1] &&
+    s[i - 2] !== s[i] &&
+    s[i - 1] !== s[i]
+}
