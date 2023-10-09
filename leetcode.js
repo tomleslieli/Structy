6346c24af378d67fb550516727bd433d83344fe4
Tom Li <tomleslieli@yahoo.com>
2023-10-08 20:22:47 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -4352,4 +4352,28 @@ function TreeMap() {
   }
 }
 
+////////// MAXIMUM POPULATION YEAR ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const maximumPopulation = function(logs) {
+  const n = logs.length
+  const arr = Array(101).fill(0)
+  const base = 1950
+  for(let log of logs) {
+    const [start, end] = log
+    arr[start - base]++
+    arr[end - base]--
+  }
+  
+  let res = 0, tmp = -Infinity
+  for(let i = 1; i < 101; i++) {
+    arr[i] += arr[i - 1]
+  }
+  for(let i = 0; i < 101; i++) {
+    if(arr[i] > tmp) {
+      res = i
+      tmp = arr[i]
+    }
+  }
+  return res + base
+};
 
