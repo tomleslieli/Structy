d6b6a211838352cbeab64ad5c1b2f6ad4a0c99bb
Tom Li <tomleslieli@yahoo.com>
2023-10-14 11:28:40 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -4420,3 +4420,16 @@ function combination(ans, comb, k, start, n) {
     comb.pop();
   }
 }
+
+////////// PARTITION ARRAY ACCORDING TO GIVEN PIVOT ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var pivotArray = function(nums, pivot) {
+  const less = [], greater = [], mid = []
+  for(const e of nums) {
+    if(e < pivot) less.push(e)
+    else if(e === pivot) mid.push(e)
+    else greater.push(e)
+  }
+ 
+ return less.concat(mid, greater)
+};
