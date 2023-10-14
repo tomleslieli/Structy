caf1afc9755b9d1ec0197f1725db71bf9d759b07
Tom Li <tomleslieli@yahoo.com>
2023-10-14 11:30:17 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -4509,3 +4509,33 @@ function isSameLine(p1, p2, p3) {
   const delta = (p3[1] - p2[1]) * (p2[0] - p1[0]) - (p2[1] - p1[1]) * (p3[0] - p2[0])
   return delta === 0
 }
+
+////////// FIND ALL LONELY NUMBERS IN THE ARRAY ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+var findLonely = function(nums) {
+  nums.sort((a, b) => a - b)
+  const cnt = {}
+  for(let e of nums) {
+    if(cnt[e] == null) cnt[e] = 0
+    cnt[e]++
+  }
+
+  const res = []
+  for(let i = 0, n = nums.length; i < n; i++) {
+    if(i === 0){
+      if(nums[i + 1] !== nums[i] + 1 && cnt[nums[i]] === 1) {
+        res.push(nums[i])  
+      }
+    } 
+    else if(i === n - 1 ) {
+      if(nums[i] !== nums[i - 1] + 1 && cnt[nums[i]] === 1) {
+        res.push(nums[i])
+      }
+    }
+    else if(cnt[nums[i]] === 1 && nums[i] !== nums[i - 1] + 1 && nums[i] !== nums[i + 1] - 1) {
+      res.push(nums[i])
+    }
+  }
+  
+  return res
+};
