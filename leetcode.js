5dc43ec1ad3cc0ed777d12ac869934280132f7ba
Tom Li <tomleslieli@yahoo.com>
2023-10-28 11:13:56 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -4736,3 +4736,26 @@ const canSeePersonsCount = function(heights) {
   
   return res
 };
+
+////////// SUM OF DIGITS OF STRING AFTER CONVERT ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const getLucky = function(s, k) {
+  let res = 0
+  const a = 'a'.charCodeAt(0)
+  const arr = []
+  for(let ch of s) {
+    arr.push(ch.charCodeAt(0) - a  + 1)
+  }
+  let str = arr.join('').split('').map(e => +e)
+  let prev = str, sum = 0
+  while(k > 0) {
+
+    let tmp = prev.reduce((ac, e) => ac + e, 0)
+
+    prev = `${tmp}`.split('').map(e => +e)
+    sum = tmp
+    k--
+  }
+  
+  return sum
+};
