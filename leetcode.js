40be95a7b8a63a4402105a48b9081dbb3d2e49dd
Tom Li <tomleslieli@yahoo.com>
2023-10-28 11:12:40 -0400

    More Problems

--- leetcode.js
+++ leetcode.js
@@ -4693,3 +4693,26 @@ const areAlmostEqual = function(s1, s2) {
   if(s1[i2] === s2[i1] && s1[i1] === s2[i2]) return true
   return false
 };
+
+////////// DESCRIBE THIS PAINTING ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const splitPainting = function(segments) {
+  const hash = {}
+  for(let [s, e, c] of segments) {
+    if(hash[s] == null) hash[s] = 0
+    if(hash[e] == null) hash[e] = 0
+    hash[s] += c
+    hash[e] -= c
+  }
+  const keys = Object.keys(hash)
+  keys.sort((a, b) => a - b)
+  let prev, color = 0
+  const res = []
+  for(let k of keys) {
+    if(prev != null && color !== 0) res.push([prev,k,color])
+
+    prev = k
+    color += hash[k]
+  } 
+  return res
+};
